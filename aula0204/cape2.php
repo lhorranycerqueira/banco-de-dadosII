1069d8b31d0ceac51c53cf6c6fa390ca15629ba3
Lhorrany <lhorranycerqueira@gmail.com>
2026-04-02 11:41:49 -0300

    exercicios 0204

---
+++ aula0204/cape2.php
@@ -0,0 +1,10 @@
+<?php
+$nome = "Maria Candida";
+$idade = 16;
+$matriculaAtiva = true;
+
+
+echo "Aluno(a): " . $nome;
+echo "\n Idade: " . $idade;
+
+?>
---
+++ aula0204/cape3.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = "Maria Candida da Silva";
+$curso = "Desenvolvimento de Sistemas";
+$semestre = 2;
+
+
+echo "Nome do Aluno: " . $nome . "\n";
+echo "Cursp: " . $curso . "\n";
+echo "Semestre: " . $semestre . "º semestre";
+?>
