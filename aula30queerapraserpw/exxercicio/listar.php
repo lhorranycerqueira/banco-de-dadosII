beb827733825a12ced3e32c40256e35965d5cab1
Lhorrany Cerqueira <lhorranycerqueira@gmail.com>
2026-05-12 07:36:01 -0300

    exercicio de delete

--- aula30queerapraserpw/exxercicio/listar.php
+++ aula30queerapraserpw/exxercicio/listar.php
@@ -7,13 +7,13 @@ $senha = '';
 
 try {
     $conexao = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $usuario, $senha);
-<<<<<<< HEAD
+
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // ✅ setAttribute
 } catch(PDOException $e) { // ✅ PDOException
-=======
+
     $conexao->setAtributte(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOExeception $e) {
->>>>>>> d47989bd7fe5623aceb9a52c663e10db25d71691
+
     die("Erro de Conexão: " . $e->getMessage()); 
 }
 
@@ -31,11 +31,11 @@ $mensagens = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <title>Painel de Mensagens</title>
     <style>
         body { font-family: Arial, sans-serif; padding: 20px; }
-<<<<<<< HEAD
+
         table { width: 100%; border-collapse: collapse; margin-top: 20px; }
-=======
+
         table { width: 100%; border-collapse: collapase; margin-top: 20px; }
->>>>>>> d47989bd7fe5623aceb9a52c663e10db25d71691
+
         th, td { border: 1px solid #ccc; padding: 10px; text-align: left; }
         th { background-color: #333; color: white; }
         tr:nth-child(even) { background-color: #f9f9f9; }
@@ -49,6 +49,7 @@ $mensagens = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <th>Nome/Sobrevivente</th>
             <th>E-mail de Contato</th>
             <th>Conteúdo da Mensagem</th>
+            <th>Ação</th>
         </tr>
         <?php foreach ($mensagens as $linha): ?>
             <tr>
@@ -56,14 +57,14 @@ $mensagens = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 <td> <?php echo $linha['nome'] ?></td>
                 <td> <?php echo $linha['email']; ?></td>
                 <td> <?php echo $linha['mensagem']; ?></td>
+                <td>
+                    <a href="deletar.php?id=<?php echo $linha['id']; ?>" class="btn-excluir" onclick="return confirm('Tem certeza que deseja excluir esse sobrevivente/mensagem?');>Excluir</a>
+                </td>
             </tr>
             <?php endforeach; ?>
     </table>
     <br>
     <a href="index.html">Voltar para o Formulário</a>
-<<<<<<< HEAD
-=======
-    <!-- o erro q ta dando Fatal error: Uncaught Error: Call to undefined method PDO::setAtributte() in C:\xampp\htdocs\projeto_site\listar.php:10 Stack trace: #0 {main} thrown in C:\xampp\htdocs\projeto_site\listar.php on line 10 -->
->>>>>>> d47989bd7fe5623aceb9a52c663e10db25d71691
+
 </body>
 </html>
