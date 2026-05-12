a7104ad04acc25934e7e40007e6cf7b9d4dcba44
Lhorrany Cerqueira <lhorranycerqueira@gmail.com>
2026-05-12 07:48:41 -0300

    delete da aula 12/05

---
+++ aula30queerapraserpw/exxercicio/deletar.php
@@ -0,0 +1,25 @@
+<?php
+$host = 'localhost';
+$dbname = 'projeto_site';
+$usuario = 'root';
+$senha = '';
+
+try {
+    $conexao = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $usuario, $senha);
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    if (Isset($_GET['id'])) {
+        $id = $_GET['id'];
+
+        $sql = "DELETE FROM contatos WHERE id = :id";
+        $stmt = $conexao->prepare($sql);
+        
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+}
+header("Location: listar.php");
+exit();
+} catch (PDOException $e) {
+    die("Erro ao tentar deletar: " . $e->getMessage());
+}
+?>
